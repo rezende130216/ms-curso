ca4e8509ab59ed8da733bc8842392d9e849505a4
rezende130216 <ra-21157141-2@alunos.unicesumar.edu.br>
2024-09-02 21:21:06 -0300

    Create project

---
+++ hr-horker/src/main/java/com/rezende/hr_horker/HrHorkerApplication.java
@@ -0,0 +1,13 @@
+package com.rezende.hr_horker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrHorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrHorkerApplication.class, args);
+	}
+
+}
