06e7f6b78b120db9b7418e7b4afa20540c3b4ecf
rezende130216 <ra-21157141-2@alunos.unicesumar.edu.br>
2024-09-08 10:59:27 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/rezende/hr_payroll/resources/PaymentResource.java
@@ -0,0 +1,24 @@
+package com.rezende.hr_payroll.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rezende.hr_payroll.entities.Payment;
+import com.rezende.hr_payroll.services.PaymentService;
+
+@RestController
+@RequestMapping(value = "/payments")
+public class PaymentResource {
+
+    @Autowired
+    private PaymentService service;
+
+    @GetMapping(value = "/{workerId}/days/{days}")
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
+        return ResponseEntity.ok(service.getPayment(workerId, days));
+    }
+}
---
+++ hr-payroll/src/main/java/com/rezende/hr_payroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.rezende.hr_payroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.rezende.hr_payroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(long workerId, int days){
+        return new Payment("Bob", 200.0, days);
+    }
+}
