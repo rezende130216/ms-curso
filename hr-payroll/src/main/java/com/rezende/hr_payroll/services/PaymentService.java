74b07239f15d68bc15bbfe21c18d53ea0aa594de
rezende130216 <ra-21157141-2@alunos.unicesumar.edu.br>
2024-09-12 20:02:43 -0300

    FeignClient

--- hr-payroll/src/main/java/com/rezende/hr_payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/rezende/hr_payroll/services/PaymentService.java
@@ -10,20 +10,16 @@ import org.springframework.web.client.RestTemplate;
 
 import com.rezende.hr_payroll.entities.Payment;
 import com.rezende.hr_payroll.entities.Worker;
+import com.rezende.hr_payroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days){
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", ""+workerId);
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
