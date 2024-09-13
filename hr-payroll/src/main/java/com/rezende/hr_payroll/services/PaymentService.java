233da3bd50c73cfcbdcd9db33b8c82ae49a67829
rezende130216 <ra-21157141-2@alunos.unicesumar.edu.br>
2024-09-12 21:07:03 -0300

    Remove imports

--- hr-payroll/src/main/java/com/rezende/hr_payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/rezende/hr_payroll/services/PaymentService.java
@@ -1,13 +1,7 @@
 package com.rezende.hr_payroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
 import com.rezende.hr_payroll.entities.Payment;
 import com.rezende.hr_payroll.entities.Worker;
 import com.rezende.hr_payroll.feignclients.WorkerFeignClient;
