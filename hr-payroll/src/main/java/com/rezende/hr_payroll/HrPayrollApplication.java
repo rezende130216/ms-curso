fe709c40e115f2e6346cbac815525b86631d2c5e
rezende130216 <ra-21157141-2@alunos.unicesumar.edu.br>
2024-09-12 21:33:39 -0300

    Adicionando microservicos no Eureka Server

--- hr-payroll/src/main/java/com/rezende/hr_payroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/rezende/hr_payroll/HrPayrollApplication.java
@@ -2,10 +2,12 @@ package com.rezende.hr_payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class HrPayrollApplication {
 
 	public static void main(String[] args) {
