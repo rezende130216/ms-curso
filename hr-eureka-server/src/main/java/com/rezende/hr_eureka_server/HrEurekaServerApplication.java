a12ac47c0e89e0d43b513ac7c8d05f5c93afb2db
rezende130216 <ra-21157141-2@alunos.unicesumar.edu.br>
2024-09-12 21:16:17 -0300

    Configure Eureka server

--- hr-eureka-server/src/main/java/com/rezende/hr_eureka_server/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/rezende/hr_eureka_server/HrEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.rezende.hr_eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class HrEurekaServerApplication {
 
 	public static void main(String[] args) {
